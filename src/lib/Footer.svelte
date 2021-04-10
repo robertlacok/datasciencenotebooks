2a46cb9db6ebc3e851ca126883f393aa1e495acf
Robert Lacok <robert@deepnote.com>
2021-04-11 00:11:34 +0200

    favicon update

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,18 +1,13 @@
 <div class="footer">
 	<div class="content">
 		<div class="pure-g">
-			<div class="pure-u-1-1">
-				<h3>Data Science Notebooks</h3>
-			</div>
-		</div>
-
-		<div class="pure-g">
 			<div class="pure-u-0 pure-u-md-4-24" />
 			<div class="pure-u-1 pure-u-md-16-24">
 				<p>
-					Data science notebooks are a new category of tools. We present a vendor neutral comparison
-					of different solutions, aiming to help data scientists pick the right tooling.
+					Data science notebooks are a new category of tools. We present a vendor neutral comparison of different
+					solutions, aiming to help data scientists pick the right tooling.
 				</p>
+				<small> © Data Science Notebooks 2021 </small>
 			</div>
 			<div class="pure-u-0 pure-u-md-4-24" />
 		</div>

52e65d4088f9f098cae5034ca77cd237cd838d46
Robert Lacok <robert@deepnote.com>
2021-04-12 13:05:11 +0200

    Add posthog

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -1,6 +1,14 @@
 <script>
+	import { onMount } from 'svelte';
 	import Footer from '$lib/Footer.svelte';
 	import '../app.scss';
+	import posthog from 'posthog-js';
+
+	onMount(() => {
+		if (!window.location.href.includes('127.0.0.1') && !window.location.href.includes('localhost')) {
+			posthog.init('Rs1YBgu0wLj-JxJseLQbaNt3h1ORj2tRfB5_7oW2owI', { api_host: 'https://app.posthog.com' });
+		}
+	});
 </script>
 
 <div class="content">

a7c96d42d889da771bdc720f7c7910d879d9fb5d
Christian Silver <christian@celadon.ai>
2022-10-14 15:27:40 +1300

    fix: use vercel env vars

--- next.config.js
+++ next.config.js
@@ -1,23 +1,8 @@
-const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
-
-function getDevelopmentServerEnv(phase) {
-  if (phase !== PHASE_DEVELOPMENT_SERVER) {
-    return null;
-  }
-
-  return {
-    NEXT_PUBLIC_URL: "http://localhost:3000",
-  };
-}
-
 module.exports = (phase, config) => {
   /** @type {import('next').NextConfig} */
   const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
-    env: {
-      ...getDevelopmentServerEnv(phase),
-    },
     rewrites: () => {
       return [
         {
--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,4 @@
-export const PUBLIC_URL: string = process.env.NEXT_PUBLIC_URL!;
-export const DEPLOYED_ENV = (process.env.NEXT_DEPLOYED_ENV ?? "development") as
-  | "production"
-  | "development";
+export const PUBLIC_URL: string = process.env.NEXT_PUBLIC_VERCEL_URL!;
+export const DEPLOYED_ENV = (process.env.NEXT_PUBLIC_VERCEL_URL ??
+  "development") as "production" | "preview" | "development";
 export const GTM_ID = DEPLOYED_ENV === "production" ? "GTM-N858BSK" : null;

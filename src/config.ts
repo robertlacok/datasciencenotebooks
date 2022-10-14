1daee07652011117bf78410c7a1c6a4e51b8a661
Christian Silver <christian@celadon.ai>
2022-10-14 15:36:43 +1300

    fix: add protocol to env var

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,14 @@
-export const PUBLIC_URL: string = process.env.NEXT_PUBLIC_VERCEL_URL!;
+const addProtocol = (url: string) => {
+  if (url.startsWith("http://") || url.startsWith("https://")) {
+    return url;
+  }
+
+  return `https://${url}`;
+};
+
+export const PUBLIC_URL: string = addProtocol(
+  process.env.NEXT_PUBLIC_VERCEL_URL!
+);
 export const DEPLOYED_ENV = (process.env.NEXT_PUBLIC_VERCEL_URL ??
   "development") as "production" | "preview" | "development";
 export const GTM_ID = DEPLOYED_ENV === "production" ? "GTM-N858BSK" : null;

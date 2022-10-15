db6a04063eeca2194ea02f5673e97541a3565f3f
Christian Silver <christian@celadon.ai>
2022-10-15 15:05:45 +1300

    fix: Vercel env var typo

--- src/config.ts
+++ src/config.ts
@@ -9,6 +9,6 @@ const addProtocol = (url: string) => {
 export const PUBLIC_URL: string = addProtocol(
   process.env.NEXT_PUBLIC_VERCEL_URL!
 );
-export const DEPLOYED_ENV = (process.env.NEXT_PUBLIC_VERCEL_URL ??
+export const DEPLOYED_ENV = (process.env.NEXT_PUBLIC_VERCEL_ENV ??
   "development") as "production" | "preview" | "development";
 export const GTM_ID = DEPLOYED_ENV === "production" ? "GTM-N858BSK" : null;

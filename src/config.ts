c36fbb1946d4a2bc2b066a3e424c6257906b065c
Christian Silver <christian@celadon.ai>
2022-10-15 17:07:16 +1300

    fix: hardcode production URL

--- src/config.ts
+++ src/config.ts
@@ -6,9 +6,14 @@ const addProtocol = (url: string) => {
   return `https://${url}`;
 };
 
-export const PUBLIC_URL: string = addProtocol(
-  process.env.NEXT_PUBLIC_VERCEL_URL!
-);
 export const DEPLOYED_ENV = (process.env.NEXT_PUBLIC_VERCEL_ENV ??
   "development") as "production" | "preview" | "development";
+
+export const PUBLIC_URL: string =
+  DEPLOYED_ENV === "production"
+    ? // We hardcode the production URL because we don't want to use the
+      // `VERCEL_URL` env var, which is a random string representing the
+      // specific deployment.
+      "https://datasciencenotebook.org"
+    : addProtocol(process.env.NEXT_PUBLIC_VERCEL_URL!);
 export const GTM_ID = DEPLOYED_ENV === "production" ? "GTM-N858BSK" : null;

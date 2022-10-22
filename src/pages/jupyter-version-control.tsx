6c87c6c67348cfbbeab9bec3f981d86db9f8b9a3
Christian Silver <christian@celadon.ai>
2022-10-22 15:21:36 +1300

    feat: draft content for version control

--- src/pages/jupyter-version-control.tsx
+++ src/pages/jupyter-version-control.tsx
@@ -1,14 +1,45 @@
-import { Heading, Text } from "@chakra-ui/react";
-import { Fragment } from "react";
+import { Box, Heading, Text } from "@chakra-ui/react";
+import { Fragment, ReactNode } from "react";
 import { ContentContainer } from "../components/ContentContainer";
 import { Seo } from "../components/Seo";
 import { SidebarLayout } from "../components/SidebarLayout";
 import { notebookToolsInCanonicalOrder } from "../notebookTools";
 
-import Content from "../content/jupyter-version-control.mdx";
+import Content, { meta } from "../content/jupyter-version-control.mdx";
 import { LargeToolCard } from "../components/LargeToolCard";
+import Image, { type ImageProps } from "next/image";
 
 interface JupyterVersionControlPageProps {}
+
+interface ProseImageProps extends Pick<ImageProps, "src" | "alt"> {
+  caption?: ReactNode;
+}
+
+function ProseImage({ src, alt, caption }: ProseImageProps) {
+  return (
+    <Box mb={4}>
+      <Box
+        borderRadius="lg"
+        borderColor="gray.200"
+        borderWidth="1px"
+        borderStyle="solid"
+        overflow="hidden"
+      >
+        <Image
+          sizes="(max-width: 800px) 100vw, 800px"
+          src={src}
+          alt={alt}
+          layout="responsive"
+        />
+      </Box>
+      {caption ? (
+        <Text mt={2} fontSize="sm" color="gray.500" textAlign="center">
+          {caption}
+        </Text>
+      ) : null}
+    </Box>
+  );
+}
 
 function JupyterVersionControlPage({}: JupyterVersionControlPageProps) {
   const tools = notebookToolsInCanonicalOrder.filter((tool) => {
@@ -18,16 +49,26 @@ function JupyterVersionControlPage({}: JupyterVersionControlPageProps) {
     const hasVersionControl = tool.features.managementVersioning?.some(
       (capability) => capability.type === "builtIn"
     );
+    const hasRealtimeCollaboration =
+      tool.features.managementCollaborativeEditing?.some(
+        (capability) => capability.type === "realtime"
+      );
 
-    return isJupyterCompatible && hasVersionControl;
+    return isJupyterCompatible && hasVersionControl && hasRealtimeCollaboration;
   });
 
   return (
     <Fragment>
       <Seo title={`Data Science Notebooks`} />
       <SidebarLayout>
-        <ContentContainer>
+        <ContentContainer maxWidth="container.md">
+          <Heading color="gray.800" as="h1" size="2xl" mb={4}>
+            {meta.title}
+          </Heading>
           <Content
+            notebookCards={tools.map((tool) => (
+              <LargeToolCard key={tool.id} tool={tool} mb={4} />
+            ))}
             components={{
               h1: (props) => (
                 <Heading color="gray.800" as="h1" size="xl" mb={4} {...props} />
@@ -38,11 +79,9 @@ function JupyterVersionControlPage({}: JupyterVersionControlPageProps) {
               p: (props) => (
                 <Text color="gray.700" as="p" size="lg" mb={4} {...props} />
               ),
+              Image: ProseImage,
             }}
           />
-          {tools.map((tool) => (
-            <LargeToolCard key={tool.id} tool={tool} mb={4} />
-          ))}
         </ContentContainer>
       </SidebarLayout>
     </Fragment>
